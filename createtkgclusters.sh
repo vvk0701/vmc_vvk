8914c75084f75748e075d46a13440aab5416d269
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-09-23 23:00:46 +0530

    Update createtkgclusters.sh

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -6,11 +6,11 @@ set -x
 for (( j=3; j<=50; j++ ))
 do
 tkg_name="tkg-cluster${j}"
-if [ $j -lt 3 ]
+if [ $j -le 3 ]
 then
     template=`cat "gcm150.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='300'
-elif [ $j -ge 4 ] && [ $j -lt 45 ]
+elif [ $j -gt 3 ] && [ $j -lt 45 ]
 then
     template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='60'
