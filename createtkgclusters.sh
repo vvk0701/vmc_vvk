9a08a6b52e21be2c1a125e7fc2687dba8275e644
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-09-28 11:37:49 +0530

    Update createtkgclusters.sh

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -13,7 +13,7 @@ then
 elif [ $j -gt 3 ] && [ $j -lt 45 ]
 then
     template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
-    sleep_var='60'
+    sleep_var='120'
 else [ $j -ge 45 ]
     a=`expr 51 - $j`
     tkg_ns="wcpns$a"
