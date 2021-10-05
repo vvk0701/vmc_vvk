101deef07c6fa6323f950f3b790afda4f14e059b
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-10-05 21:59:02 +0530

    Update createtkgclusters.sh

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -14,13 +14,13 @@ then
 elif [ $j -gt 3 ] && [ $j -lt 45 ]
 then
     template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
-    sleep_var='120'
+    sleep_var='320'
 else [ $j -ge 45 ]
     a=3
     a=a+`expr 51 - $j`
     tkg_ns="wcpns$a"
     template=`cat "gcm4.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
-    sleep_var='120'
+    sleep_var='420'
 fi
 echo "$template" | kubectl apply -f -
 output=`kubectl get tkc`
