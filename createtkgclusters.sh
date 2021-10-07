099528d4c15ef8788350cb11816f5ff7009c9eaa
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-10-07 13:00:17 +0530

    Update createtkgclusters.sh

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -11,9 +11,13 @@ then
     tkg_ns="wcpns$j"
     template=`cat "gcm150.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='900'
-elif [ $j -gt 3 ] && [ $j -lt 45 ]
+elif [ $j -gt 3 ] && [ $j -lt 25 ]
 then
     template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
+    sleep_var='320'
+elif [ $j -gt 24 ] && [ $j -lt 45 ]
+then
+    template=`cat "gcm2_alpha2_g.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='320'
 else [ $j -ge 45 ]
     a=3
