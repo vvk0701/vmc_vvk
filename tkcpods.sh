5cf44ece654b9b86873689e2e4e9930ef9a8038e
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-10-28 17:23:20 +0530

    Update tkcpods.sh

--- tkcpods.sh
+++ tkcpods.sh
@@ -3,11 +3,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 set -x
-#kubectl apply -f psp.yaml
+kubectl apply -f psp.yaml
 for (( i=1; i<=800; i++ ))
 do
 echo "creating tkc namespace "
-#kubectl create ns tkc-$i
+kubectl create ns tkc-$i
 echo "create tkc pods"
 kubectl apply -f replicaset.yaml -n tkc-$i
 sleep 5
