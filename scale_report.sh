26896ac77ea22f66867cf4396b1ec1f407350146
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-11-10 09:59:31 +0530

    Create scale_report.sh

---
+++ scale_report.sh
@@ -0,0 +1,25 @@
+KUBECTL_VSPHERE_PASSWORD=$1;
+LARGETKG="tkg-cluster50";
+LARGETKGNS="wcpns6";
+kubectl vsphere login --server=192.168.123.2 -u cloudadmin@vmc.local --insecure-skip-tls-verify;
+
+echo "Number of NameSpaces: "
+kubectl get ns | grep wcpns | wc -l;
+
+echo "Number of TKC Clusters: "
+kubectl get tkc -A | grep tkg | wc -l;
+
+echo "Login to Large TKC Cluster"
+
+kubectl vsphere login --server=192.168.123.2 -u cloudadmin@vmc.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name $LARGETKG --tanzu-kubernetes-cluster-namespace $LARGETKGNS
+
+echo "Number of Running Nginx Pods: "
+kubectl get pods -A | grep "nginx" | grep "Running" | wc -l;
+
+echo "Number of Nginx pods in Pending state: "
+kubectl get pods -A | grep "nginx" | grep "Pending" | wc -l;
+
+kubectl vsphere login --server=192.168.123.2 -u cloudadmin@vmc.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name tkg-cluster2 --tanzu-kubernetes-cluster-namespace wcpns2
+
+echo "Number of PVC's in tkg-cluster6 of NS wcpns4"
+kubectl get pvc -A | wc -l;
