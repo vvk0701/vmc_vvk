99dd9c3176688746b9f76539815617868abbb9f3
Manasa1702 <94034067+Manasa1702@users.noreply.github.com>
2022-02-04 13:49:06 +0530

    Update createpvc.sh

--- createpvc.sh
+++ createpvc.sh
@@ -7,7 +7,7 @@ for (( j=1; j<=60; j++ ))
 do
     pvc_name="pvc${j}"
     template=`cat "pvc.yaml" |sed "s/{{pvc_name}}/$pvc_name/g"`
-    echo "$template" | kubectl delete -f -
+    echo "$template" | kubectl apply -f -
     output=`kubectl get pvc`
     date_time=`date`
     sleep "1m"
