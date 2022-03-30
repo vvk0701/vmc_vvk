ecff31ee40c883a7eb959ab8344dc2ffeff03df0
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2022-03-30 23:40:46 +0530

    Update createpvc.sh

--- createpvc.sh
+++ createpvc.sh
@@ -4,12 +4,12 @@ set -o pipefail
 set -o nounset
 set -x
 ns=$1
-for (( j=1; j<=60; j++ ))
+for (( j=1; j<=225; j++ ))
 do
     pvc_name="pvc${j}"
     template=`cat "pvc.yaml" |sed "s/{{pvc_name}}/$pvc_name/g" | sed "s/{{ns}}/$ns/g"`
     echo "$template" | kubectl apply -f -
     output=`kubectl get pvc`
     date_time=`date`
-    sleep "1m"
+    sleep "30s"
 done
