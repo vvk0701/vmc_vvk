35b2f22e0f3e3f9cdee7aaf2e7f61efa3a06441f
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2022-03-31 00:29:16 +0530

    Update Pipeline_PVC.groovy

--- Pipeline_PVC.groovy
+++ Pipeline_PVC.groovy
@@ -13,7 +13,7 @@ pipeline {
         booleanParam(name: 'dryrun', defaultValue: true, description: 'Boolean flag for populating params')
 	      string(name: 'SV_Hostname', defaultValue: '', description: 'Enter Supervisor Cluster Hostname')
         string(name: 'Kubectl_Password', defaultValue: '', description: 'Enter Password from Cloudadmin user')
-	      integer(name: 'ns_count', defaultValue: '10', description: 'Enter  WCP Namespace Where PVCs have to created')
+	      string(name: 'ns_count', defaultValue: '10', description: 'Enter  WCP Namespace Where PVCs have to created')
 		    	     
     }
     
@@ -36,7 +36,7 @@ pipeline {
 	    
 	stage('PVC Creation:'){
             steps{
-		    for (int i=1; i<ns_count; i++){
+		    for (int i=1; i<int(ns_count); i++){
     			def wcpns='wcpns'+i
 			createPvc(params.Kubectl_Password, wcpns, params.SV_Hostname)
 		    }
