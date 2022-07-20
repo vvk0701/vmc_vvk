adc976a8ce386a0ea45cb30c2084810e866dcd0c
Manasa1702 <94034067+Manasa1702@users.noreply.github.com>
2022-07-20 22:35:35 +0530

    Update createtkgclusters.sh

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -10,8 +10,8 @@ do
 tkg_name="tkg-cluster${j}"
 if [ $j -le 5 ]
 then
-    tkg_ns="testns$j"
-    template=`cat "gcm46.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
+    tkg_ns="wcpns$j"
+    template=`cat "gcm150.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='420'
 elif [ $j -gt 5 ] && [ $j -lt 25 ]
 then
@@ -23,7 +23,7 @@ then
     sleep_var='240'
 else [ $j -ge 47 ]
     a=`expr 56 - $j`
-    tkg_ns="testns$a"
+    tkg_ns="wcpns$a"
     template=`cat "gcm150.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='900'
 fi
