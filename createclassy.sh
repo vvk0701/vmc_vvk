1b03667c5d50273f74b612c5136b8adcfad7a349
Manasa1702 <94034067+Manasa1702@users.noreply.github.com>
2023-08-30 18:30:51 +0530

    Update createclassy.sh

--- createclassy.sh
+++ createclassy.sh
@@ -11,21 +11,21 @@ tkg_name="classy-cluster${j}"
 if [ $j -le 5 ]
 then
     tkg_ns="wcpns$j"
-    template=`cat "test.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
+    template=`cat "classy.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='420'
 elif [ $j -gt 5 ] && [ $j -lt 10 ]
 then
     tkg_ns="wcpns6"
-    template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
+    template=`cat "classy.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='240'
 elif [ $j -gt 9 ] && [ $j -lt 19 ]
 then
     tkg_ns="wcpns4"
-    template=`cat "gcm2_alpha2_g.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
+    template=`cat "classy.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='240'
 else [ $j -ge 19 ]
     tkg_ns="wcpns3"
-    template=`cat "gcm150.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
+    template=`cat "classy.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='900'
 fi
 echo "$template" | kubectl apply -f -
