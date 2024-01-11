c8f9c9f086201948981d002e4d09a69fc698ec11
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2024-01-11 14:18:34 +0530

    Update createtkgclusters.sh
    Re arranged distribution

--- createtkgclusters.sh
+++ createtkgclusters.sh
@@ -4,24 +4,24 @@ set -o pipefail
 set -o nounset
 set -x
 start=$1
-end=48
+end=50
 for (( j=$start; j<=$end; j++ ))
 do
 tkg_name="tkg-cluster${j}"
-if [ $j -le 5 ]
+if [ $j -le 9 ]
 then
     tkg_ns="wcpns$j"
     template=`cat "test.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
     sleep_var='420'
-elif [ $j -gt 5 ] && [ $j -lt 25 ]
+elif [ $j -gt 9 ] && [ $j -lt 25 ]
 then
     template=`cat "gcm2_alpha2.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='240'
-elif [ $j -gt 24 ] && [ $j -lt 47 ]
+elif [ $j -gt 24 ] && [ $j -lt 48 ]
 then
     template=`cat "gcm2_alpha2_g.yaml" |sed "s/{{MY_NAME}}/$tkg_name/g"`
     sleep_var='240'
-else [ $j -ge 47 ]
+else [ $j -ge 49 ]
     a=`expr 56 - $j`
     tkg_ns="wcpns$a"
     template=`cat "gcm150.yaml" | sed "s/{{MY_NAME}}/$tkg_name/g" | sed "s/{{MY_NS}}/$tkg_ns/g"`
