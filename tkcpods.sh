80614ef948a841e6d9a335bda4c114c03d41014c
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2024-03-26 14:46:50 +0530

    Update tkcpods.sh

--- tkcpods.sh
+++ tkcpods.sh
@@ -3,10 +3,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 set -x
-for (( i=135; i<=800; i++ ))
+for (( i=1; i<=800; i++ ))
 do
 echo "creating tkc namespace "
 kubectl create ns tkc-$i
+kubectl label --overwrite ns tkc-$i pod-security.kubernetes.io/enforce=privileged
 echo "create tkc pods"
 kubectl apply -f replicaset.yaml -n tkc-$i
 sleep 5
